b2f0c6412e81cf2925e22f50da90f0bb29df9e62
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-25 15:34:11 +0300

    Add project files.

---
+++ HelloApi/Controllers/HelloController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+      
+    }
+}
